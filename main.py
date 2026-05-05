f91982f9cbb192e3051612a8a0aa9d6e55aef396
KoLwin55 <htetmyat404k@gmail.com>
2026-05-06 02:38:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
-import SMM_V5_2
+import SMPL_v5_2 
 
 if __name__ == "__main__":
     try:
-        SMM_V5_2.main()
+        SMPL_v5_2.main()
     except KeyboardInterrupt:
         print("\n[!] Exiting...")
